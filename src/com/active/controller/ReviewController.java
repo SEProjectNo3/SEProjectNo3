ea90968a7f9e42f0d454dfbe64e439fd8df5a695
kso0611 <kso0611@gmail.com>
2016-11-24 20:22:21 +0900

    Comment, Enroll and ReviewController is semi-completed.

--- src/com/active/controller/ReviewController.java
+++ src/com/active/controller/ReviewController.java
@@ -20,6 +20,7 @@ import com.active.model.User;
 public class ReviewController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
